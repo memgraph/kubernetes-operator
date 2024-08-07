2fd407259aa4e1cf4e8981cd2200e95c369e4147
Andi Skrgat <andi8647@gmail.com>
2024-08-07 15:19:31 +0200

    Add reconciliation of data instances services

--- internal/controller/memgraphha_controller.go
+++ internal/controller/memgraphha_controller.go
@@ -136,6 +136,29 @@ func (r *MemgraphHAReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 	logger.Info("Reconciliation of coordinators finished without actions needed.")
 
 	for dataInstanceId := 0; dataInstanceId <= 1; dataInstanceId++ {
+		// ClusterIP
+		dataInstanceClusterIPStatus, dataInstanceClusterIPErr := r.reconcileDataInstanceClusterIPService(ctx, memgraphha, &logger, dataInstanceId)
+		if dataInstanceClusterIPErr != nil {
+			logger.Info("Error returned when reconciling ClusterIP with id", dataInstanceId, "Returning empty Result with error.")
+			return ctrl.Result{}, dataInstanceClusterIPErr
+		}
+
+		if dataInstanceClusterIPStatus == true {
+			logger.Info("ClusterIP with id", dataInstanceId, "has been created. Returning Result with the request for requeing with error set to nil.")
+			return ctrl.Result{Requeue: true}, nil
+		}
+
+		// NodePort
+		dataInstanceNodePortStatus, dataInstanceNodePortErr := r.reconcileDataInstanceNodePortService(ctx, memgraphha, &logger, dataInstanceId)
+		if dataInstanceNodePortErr != nil {
+			logger.Info("Error returned when reconciling NodePort with id", dataInstanceId, "Returning empty Result with error.")
+			return ctrl.Result{}, dataInstanceNodePortErr
+		}
+
+		if dataInstanceNodePortStatus == true {
+			logger.Info("NodePort with id", dataInstanceId, "has been created. Returning Result with the request for requeing with error set to nil.")
+			return ctrl.Result{Requeue: true}, nil
+		}
 
 		// Data instance
 		dataInstancesStatus, dataInstancesErr := r.reconcileDataInstance(ctx, memgraphha, &logger, dataInstanceId)
@@ -154,6 +177,136 @@ func (r *MemgraphHAReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 
 	// The resource doesn't need to be reconciled anymore
 	return ctrl.Result{}, nil
+}
+
+func (r *MemgraphHAReconciler) reconcileDataInstanceNodePortService(ctx context.Context, memgraphha *memgraphv1.MemgraphHA, logger *logr.Logger, dataInstanceId int) (bool, error) {
+	serviceName := fmt.Sprintf("memgraph-data-%d-external", dataInstanceId)
+	logger.Info("Started reconciling NodePort service", serviceName)
+
+	dataInstanceNodePortService := &corev1.Service{}
+	err := r.Get(ctx, types.NamespacedName{Name: serviceName, Namespace: memgraphha.Namespace}, dataInstanceNodePortService)
+
+	if err == nil {
+		logger.Info("NodePort", serviceName, "already exists.")
+		return false, nil
+	}
+
+	if errors.IsNotFound(err) {
+		nodePort := r.createDataInstanceNodePort(memgraphha, dataInstanceId)
+		logger.Info("Creating a new NodePort", "NodePort.Namespace", nodePort.Namespace, "NodePort.Name", nodePort.Name)
+		err := r.Create(ctx, nodePort)
+		if err != nil {
+			logger.Error(err, "Failed to create new NodePort", "NodePort.Namespace", nodePort.Namespace, "NodePort.Name", nodePort.Name)
+			return true, err
+		}
+		logger.Info("NodePort", serviceName, "is created.")
+		return true, nil
+	}
+
+	logger.Error(err, "Failed to fetch NodePort", serviceName)
+	return true, err
+
+}
+
+func (r *MemgraphHAReconciler) createDataInstanceNodePort(memgraphha *memgraphv1.MemgraphHA, dataInstanceId int) *corev1.Service {
+	serviceName := fmt.Sprintf("memgraph-data-%d-external", dataInstanceId)
+	dataInstanceName := fmt.Sprintf("memgraph-data-%d", dataInstanceId)
+	// TODO: (andi) Extract somehow configuration and move into separate files.
+	boltPort := 7687
+
+	dataInstanceNodePort := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      serviceName,
+			Namespace: memgraphha.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Type:     corev1.ServiceTypeNodePort,
+			Selector: createDataInstanceLabels(dataInstanceName),
+			Ports: []corev1.ServicePort{
+				{
+					Name:       "bolt",
+					Protocol:   corev1.ProtocolTCP,
+					Port:       int32(boltPort),
+					TargetPort: intstr.FromInt(boltPort),
+				},
+			},
+		},
+	}
+
+	ctrl.SetControllerReference(memgraphha, dataInstanceNodePort, r.Scheme)
+	return dataInstanceNodePort
+}
+
+func (r *MemgraphHAReconciler) reconcileDataInstanceClusterIPService(ctx context.Context, memgraphha *memgraphv1.MemgraphHA, logger *logr.Logger, dataInstanceId int) (bool, error) {
+	serviceName := fmt.Sprintf("memgraph-data-%d", dataInstanceId)
+	logger.Info("Started reconciling ClusterIP service", serviceName)
+
+	dataInstanceClusterIPService := &corev1.Service{}
+	err := r.Get(ctx, types.NamespacedName{Name: serviceName, Namespace: memgraphha.Namespace}, dataInstanceClusterIPService)
+
+	if err == nil {
+		logger.Info("ClusterIP", serviceName, "already exists.")
+		return false, nil
+	}
+
+	if errors.IsNotFound(err) {
+		clusterIP := r.createDataInstanceClusterIP(memgraphha, dataInstanceId)
+		logger.Info("Creating a new ClusterIP", "ClusterIP.Namespace", clusterIP.Namespace, "ClusterIP.Name", clusterIP.Name)
+		err := r.Create(ctx, clusterIP)
+		if err != nil {
+			logger.Error(err, "Failed to create new ClusterIP", "ClusterIP.Namespace", clusterIP.Namespace, "ClusterIP.Name", clusterIP.Name)
+			return true, err
+		}
+		logger.Info("ClusterIP", serviceName, "is created.")
+		return true, nil
+	}
+
+	logger.Error(err, "Failed to fetch ClusterIP", serviceName)
+	return true, err
+
+}
+
+func (r *MemgraphHAReconciler) createDataInstanceClusterIP(memgraphha *memgraphv1.MemgraphHA, dataInstanceId int) *corev1.Service {
+	serviceName := fmt.Sprintf("memgraph-data-%d", dataInstanceId)
+	dataInstanceName := serviceName
+	// TODO: (andi) Extract somehow configuration and move into separate files.
+	boltPort := 7687
+	replicationPort := 20000
+	mgmtPort := 10000
+
+	dataInstanceClusterIP := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      serviceName,
+			Namespace: memgraphha.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Type:     corev1.ServiceTypeClusterIP,
+			Selector: createDataInstanceLabels(dataInstanceName),
+			Ports: []corev1.ServicePort{
+				{
+					Name:       "bolt",
+					Protocol:   corev1.ProtocolTCP,
+					Port:       int32(boltPort),
+					TargetPort: intstr.FromInt(boltPort),
+				},
+				{
+					Name:       "replication",
+					Protocol:   corev1.ProtocolTCP,
+					Port:       int32(replicationPort),
+					TargetPort: intstr.FromInt(replicationPort),
+				},
+				{
+					Name:       "management",
+					Protocol:   corev1.ProtocolTCP,
+					Port:       int32(mgmtPort),
+					TargetPort: intstr.FromInt(mgmtPort),
+				},
+			},
+		},
+	}
+
+	ctrl.SetControllerReference(memgraphha, dataInstanceClusterIP, r.Scheme)
+	return dataInstanceClusterIP
 }
 
 func (r *MemgraphHAReconciler) reconcileDataInstance(ctx context.Context, memgraphha *memgraphv1.MemgraphHA, logger *logr.Logger, dataInstanceId int) (bool, error) {
@@ -358,7 +511,7 @@ func (r *MemgraphHAReconciler) reconcileCoordNodePortService(ctx context.Context
 	}
 
 	if errors.IsNotFound(err) {
-		nodePort := r.createNodePort(memgraphha, coordId)
+		nodePort := r.createCoordNodePort(memgraphha, coordId)
 		logger.Info("Creating a new NodePort", "NodePort.Namespace", nodePort.Namespace, "NodePort.Name", nodePort.Name)
 		err := r.Create(ctx, nodePort)
 		if err != nil {
@@ -374,7 +527,7 @@ func (r *MemgraphHAReconciler) reconcileCoordNodePortService(ctx context.Context
 
 }
 
-func (r *MemgraphHAReconciler) createNodePort(memgraphha *memgraphv1.MemgraphHA, coordId int) *corev1.Service {
+func (r *MemgraphHAReconciler) createCoordNodePort(memgraphha *memgraphv1.MemgraphHA, coordId int) *corev1.Service {
 	serviceName := fmt.Sprintf("memgraph-coordinator-%d-external", coordId)
 	coordName := fmt.Sprintf("memgraph-coordinator-%d", coordId)
 	// TODO: (andi) Extract somehow configuration and move into separate files.
